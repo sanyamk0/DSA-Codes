a0f71f9c645d59d43a63eb7e827bd815e97fcad4
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-05-17 22:55:10 +0530

    Added Sort 0's and 1's Code

---
+++ sort0and1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void sortOne(int arr[], int n)
+{
+    int left = 0;
+    int right = n - 1;
+    while (left < right)
+    {
+        while (arr[left] == 0 && left < right)
+        {
+            left++;
+        }
+        while (arr[right] == 1 && left < right)
+        {
+            right--;
+        }
+        // when arr[left]==1 and arr[right]==0
+        if (left < right)
+        {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[10] = {1, 0, 0, 0, 1, 1, 0, 1, 1, 0};
+
+    sortOne(arr, 10);
+    printArray(arr, 10);
+    return 0;
+}
