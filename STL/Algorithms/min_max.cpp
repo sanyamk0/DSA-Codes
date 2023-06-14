33c8e2a079226d1530b9172202c2878dbc180e7f
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-06-14 14:43:47 +0530

    Learning STL min_max

---
+++ STL/Algorithms/min_max.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a = 3, b = 5;
+    cout << min(a, b) << endl;
+    cout << max(a, b) << endl;
+    return 0;
+}
