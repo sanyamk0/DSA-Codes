e6722496404fb3162ff9c0013b3f736919fc5d18
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-06-14 13:19:33 +0530

    Learning STL List

---
+++ STL/list.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+    list<int> l;
+    l.push_back(1);
+    l.push_front(2);
+    for (int i : l)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "Size of List " << l.size();
+    cout << endl;
+    cout << "Before Erase" << endl;
+    for (int i : l)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    l.erase(l.begin());
+    cout << "After Erase" << endl;
+    for (int i : l)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
