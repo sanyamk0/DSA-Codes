a18bd09c5314a44535ad2f02c48d2ed5556971a2
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-06-28 21:45:02 +0530

    Learning Recursion

--- Recursion/sumArray.cpp
+++ Recursion/sumArray.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int findSum(int arr[], int size)
 {
+    if (size == 0)
+        return 0;
     if (size == 1)
         return arr[0];
     int ans = arr[0] + findSum(arr + 1, size - 1);
