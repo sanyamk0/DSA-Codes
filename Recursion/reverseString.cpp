7ef35069cf4e9b33a4f55a92cc392124bffb7b44
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-06-29 13:40:47 +0530

    Updated Code

--- Recursion/reverseString.cpp
+++ Recursion/reverseString.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void reverse(string &s, int i, int j)
+// void reverse(string &s, int i, int j)
+// {
+//     if (i > j)
+//         return;
+//     swap(s[i], s[j]);
+//     i++, j--;
+//     reverse(s, i, j);
+// }
+void reverse(string &s, int i)
 {
-    if (i > j)
+    int n = s.length();
+    if (i > n - i - 1)
         return;
-    swap(s[i], s[j]);
-    i++, j--;
-    reverse(s, i, j);
+    swap(s[i], s[n - i - 1]);
+    i++;
+    reverse(s, i);
 }
 int main()
 {
     string s = "abcdefghij";
-    reverse(s, 0, s.length() - 1);
+    // reverse(s, 0, s.length() - 1);
+    reverse(s, 0);
     cout << s << endl;
     return 0;
 }
